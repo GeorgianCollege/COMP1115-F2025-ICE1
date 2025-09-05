3f0d740f313aa87c0d728cc44077645e8c3c6fea
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2025-09-05 14:39:26 -0400

    Added Comment Header

--- ICE1/Program.cs
+++ ICE1/Program.cs
@@ -1,9 +1,17 @@
-﻿namespace ICE1
+﻿/*
+ * Name: Tom Tsiliopoulos
+ * Date: September 5, 2025
+ * ICE 1
+ */
+
+
+namespace ICE1
 {
     internal class Program
     {
         static void Main(string[] args) 
         {
+
             Console.WriteLine("Hi There!");
             Console.WriteLine("My Name is Tom.");
             Console.WriteLine("I'm excited to meet you!");
